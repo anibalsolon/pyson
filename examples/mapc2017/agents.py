d79364102eaa7d0a10916788f3848a63f8c78c0b
anibalsolon <anibalsolon@gmail.com>
2018-02-21 10:24:50 -0300

    move mapc agent to examples

--- examples/mapc2017/agents.py
+++ examples/mapc2017/agents.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 
 import asyncio
-import pyson.mapc2017
 import pyson.runtime
 import logging
+from mapc2017 import (actions, Agent, Environment)
 
 
 pyson.get_logger("pyson.mapc2017").setLevel(logging.DEBUG)
 
 
 async def main():
-    env = pyson.mapc2017.Environment()
+    env = Environment()
 
     with open("agent1.asl") as source:
-        agent1 = env.build_agent(source, pyson.mapc2017.actions, pyson.mapc2017.Agent, "agentA1")
+        agent1 = env.build_agent(source, actions, Agent, "agentA1")
     with open("agent1.asl") as source:
-        agent2 = env.build_agent(source, pyson.mapc2017.actions, pyson.mapc2017.Agent, "agentA2")
+        agent2 = env.build_agent(source, actions, Agent, "agentA2")
     with open("agent1.asl") as source:
-        agent3 = env.build_agent(source, pyson.mapc2017.actions, pyson.mapc2017.Agent, "agentA3")
+        agent3 = env.build_agent(source, actions, Agent, "agentA3")
     with open("agent1.asl") as source:
-        agent4 = env.build_agent(source, pyson.mapc2017.actions, pyson.mapc2017.Agent, "agentA4")
+        agent4 = env.build_agent(source, actions, Agent, "agentA4")
     with open("agent1.asl") as source:
-        agent5 = env.build_agent(source, pyson.mapc2017.actions, pyson.mapc2017.Agent, "agentA5")
+        agent5 = env.build_agent(source, actions, Agent, "agentA5")
     with open("agent1.asl") as source:
-        agent6 = env.build_agent(source, pyson.mapc2017.actions, pyson.mapc2017.Agent, "agentA6")
+        agent6 = env.build_agent(source, actions, Agent, "agentA6")
 
     await agent1.connect("agentA1", "1")
     #await agent2.connect("agentA2", "1")
