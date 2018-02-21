7b4aae3902efadafeb2d8821d9514f7494e0856d
anibalsolon <anibalsolon@gmail.com>
2018-02-21 13:11:57 -0300

    custom communication case to trigger old naming bug

--- examples/communication/env.py
+++ examples/communication/env.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
+import os
 import pyson
 import pyson.runtime
 import pyson.stdlib
 
-import os
-
-
 env = pyson.runtime.Environment()
 
 with open(os.path.join(os.path.dirname(__file__), "receiver.asl")) as source:
-    agents = env.build_agents(source, 3, pyson.stdlib.actions)
+    env.build_agent(source, pyson.stdlib.actions)
+
+with open(os.path.join(os.path.dirname(__file__), "receiver.asl")) as source:
+    agents = env.build_agents(source, 3, pyson.stdlib.actions, name="custom_receiver")
 
 with open(os.path.join(os.path.dirname(__file__), "sender.asl")) as source:
     agents.append(env.build_agent(source, pyson.stdlib.actions))
